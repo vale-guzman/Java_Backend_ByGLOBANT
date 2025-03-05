f5b42d711122ad4ebf2fe789591cc5396602c416
“mvale888” <mvale888@gmail.com>
2025-03-05 18:51:39 -0300

    Add EditorialController + html correspondiente

---
+++ SPRING/biblioteca/src/main/java/com/egg/biblioteca/controllers/EditorialControlador.java
@@ -0,0 +1,38 @@
+package com.egg.biblioteca.controllers;
+
+import com.egg.biblioteca.exceptions.MyException;
+import com.egg.biblioteca.services.EditorialService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Controller
+@RequestMapping("/editorial") // http://localhost:8080/editorial
+public class EditorialControlador {
+
+    @Autowired
+    private EditorialService editorialService;
+
+
+    @GetMapping("/registrar") // http://localhost:8080/editorial/registrar
+    public String registrar(){
+        return "editorial_form.html";
+    }
+
+    @PostMapping("/registro")
+    public String registro(@RequestParam String nombre){
+       try {
+        editorialService.crearEditorial(nombre);
+    }catch (MyException ex){
+           Logger.getLogger(EditorialControlador.class.getName()).log(Level.SEVERE,null, ex);
+           return "editorial_form.html";
+    }
+        return "index.html";
+}
+}
