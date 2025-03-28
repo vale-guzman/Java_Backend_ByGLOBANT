e561eece8fd2834ef81cc9b02264f8ef1b492479
“mvale888” <mvale888@gmail.com>
2025-03-27 21:43:16 -0300

    Fix funcionalidad 'Modificar Usuario'

--- SPRING/biblioteca/src/main/java/com/egg/biblioteca/controllers/AdminControlador.java
+++ SPRING/biblioteca/src/main/java/com/egg/biblioteca/controllers/AdminControlador.java
@@ -2,13 +2,14 @@ package com.egg.biblioteca.controllers;
 
 import com.egg.biblioteca.entities.Libro;
 import com.egg.biblioteca.entities.Usuario;
+import com.egg.biblioteca.exceptions.MyException;
 import com.egg.biblioteca.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.UUID;
@@ -20,21 +21,48 @@ public class AdminControlador {
     @Autowired
     private UsuarioService usuarioService;
 
-    @GetMapping ("/dashboard")  //Por protocolo, para aquellos paneles que utilizan los administradores.
-    public String panelAdministrativo(){
-        return  "panel_form.html";
+    @GetMapping("/dashboard")  //Por protocolo, para aquellos paneles que utilizan los administradores.
+    public String panelAdministrativo() {
+        return "panel_form.html";
     }
 
     @GetMapping("/usuarios") // http://localhost:8080/admin/usuarios
-    public String listar(ModelMap modelMap){
+    public String listar(ModelMap modelMap) {
         List<Usuario> usuarios = usuarioService.listarUsuarios();
         modelMap.addAttribute("usuarios", usuarios);
         return "usuario_list.html";
     }
 
     @GetMapping("modificarRol/{id}")
-    public String cambiarRol(@PathVariable UUID id){
+    public String cambiarRol(@PathVariable UUID id) {
         usuarioService.cambiarRol(id);
         return "redirect:/admin/usuarios";
+    }
+    @GetMapping("/modificar/{id}")
+    public String modificarUsuario(@PathVariable UUID id, ModelMap modelo){
+        Usuario usuario = usuarioService.getOne(id);
+        modelo.addAttribute("usuario", usuario);
+
+        return "usuario_modificar.html";
+    }
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    @PostMapping("/modificar/{id}")
+    public String actualizar(MultipartFile archivo, @PathVariable UUID id, @RequestParam String nombre, @RequestParam String email,
+                             @RequestParam String password, @RequestParam String password2, ModelMap modelo) {
+
+        try {
+            usuarioService.actualizar(archivo, id, nombre, email, password, password2);
+            modelo.put("exito", "El usuario fue actualizado correctamente.");
+            return "redirect:/admin/usuarios";
+
+        } catch (MyException ex) {
+            modelo.put("error", ex.getMessage());
+            modelo.put("nombre", nombre);
+            modelo.put("email", email);
+            modelo.put("password",password);
+            modelo.put("password2",password2);
+            return "usuario_modificar.html";
+        }
+
     }
 }
--- SPRING/biblioteca/src/main/java/com/egg/biblioteca/controllers/PortalControlador.java
+++ SPRING/biblioteca/src/main/java/com/egg/biblioteca/controllers/PortalControlador.java
@@ -87,23 +87,6 @@ public class PortalControlador {
         return "usuario_modificar.html";
     }
 
-    @PreAuthorize("hasAnyRole('ROLE_USER','ROLE_ADMIN')")
-    @PostMapping("/perfil/{id}")
-    public String actualizar(MultipartFile archivo, @PathVariable UUID id, @RequestParam String nombre, @RequestParam String email,
-                             @RequestParam String password, @RequestParam String password2, ModelMap modelo) {
-
-        try {
-            usuarioService.actualizar(archivo, id, nombre, email, password, password2);
-            modelo.put("exito", "El usuario fue actualizado correctamente.");
-            return "inicio.html";
-        } catch (MyException ex) {
-            modelo.put("error", ex.getMessage());
-            modelo.put("nombre", nombre);
-            modelo.put("email", email);
-
-            return "usuario_modificar.html";
-        }
-    }
     @GetMapping("/logout")
     public String logout() {
         return "login.html";
