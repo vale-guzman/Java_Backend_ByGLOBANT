884a1d80e74845680b854f76c26e5652d46febce
“mvale888” <mvale888@gmail.com>
2025-04-03 17:13:30 -0300

    Add Entities

--- bibliotecaAPI/src/main/java/com/egg/bibliotecaAPI/entities/Author.java
+++ bibliotecaAPI/src/main/java/com/egg/bibliotecaAPI/entities/Author.java
@@ -1,4 +1,22 @@
 package com.egg.bibliotecaAPI.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name="authors")
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    String nombre;
+    boolean activo =true;
 }
--- bibliotecaAPI/src/main/java/com/egg/bibliotecaAPI/entities/Book.java
+++ bibliotecaAPI/src/main/java/com/egg/bibliotecaAPI/entities/Book.java
@@ -1,4 +1,31 @@
 package com.egg.bibliotecaAPI.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name="books")
 public class Book {
+    @Id
+    @Column(unique = true)
+    private long isbn;
+
+    private String titulo;
+    private int ejemplares;
+    private boolean activo=true;
+
+    @ManyToOne
+    @JoinColumn(name = "id_autor")
+    private Author autor;
+
+    @ManyToOne
+    @JoinColumn(name = "id_editorial")
+    private Publisher editorial;
 }
