e4f3c558b4fc25b49289ef8378105012f95cef9b
“mvale888” <mvale888@gmail.com>
2025-04-03 14:46:32 -0300

    Create a new proyect bibliotecaAPI, a REST API for Library management, built with Spring and Maven.

---
+++ SPRING/bibliotecaAPI/src/test/java/com/egg/bibliotecaAPI/BibliotecaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egg.bibliotecaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
